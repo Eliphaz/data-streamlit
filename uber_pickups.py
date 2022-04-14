3c068e5073272bdd8e1f8f4dab5f62a3db9b5135
Eliphaz <eliahlander3@gmail.com>
2022-04-14 14:19:31 -0600

    added mpl pie charts

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Uber pickups in NYC')
 
@@ -37,3 +38,40 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+
+st.subheader('line chart of hourly pickups')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+st.line_chart(data=hist_values, width=0, height=0, use_container_width=True)
+
+
+labels = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0,
+          '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0}
+for i in data[DATE_COLUMN]:
+    for j in range(24):
+        if i.hour == j:
+            labels[str(j)] += 1
+
+fig1, ax1 = plt.subplots()
+ax1.pie(labels.values(), labels=labels.keys(), autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.subheader('matplotlib pie schart of uber rides by hour')
+st.pyplot(fig1)
+
+am_pm = {'am': 0, 'pm': 0}
+for i in data[DATE_COLUMN]:
+    for j in range(24):
+        if i.hour < 12:
+            am_pm['am'] += 1
+        elif i.hour > 12:
+            am_pm['pm'] += 1
+
+fig2, ax2 = plt.subplots()
+ax2.pie(am_pm.values(), labels=am_pm.keys(), autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.subheader('rides by am/pm')
+st.pyplot(fig2)
